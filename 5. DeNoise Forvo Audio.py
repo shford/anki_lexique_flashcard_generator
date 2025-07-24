e372fb53367ee6107ed800e12eb18044006504aa
hampton.ford <stevenhford3@gmail.com>
2025-07-24 01:03:03 -0600

    added config file and reset sane configs... you can probably see the relationship here

--- 5. DeNoise Forvo Audio.py
+++ 5. DeNoise Forvo Audio.py
@@ -20,6 +20,7 @@ Additional notes:
     I would not recommend running this repeatedly as data is lost everytime audio is filtered. Over repeated
     runs data may lose quality.
 """
+from ast import literal_eval
 import datetime
 import io
 import os
@@ -36,8 +37,8 @@ import soundfile as sf
 from scipy.signal import medfilt
 
 # ==== Configuration ====
-BACKUP = False                      # todo toggle to True prior to pushing
-CHECK_FOR_CORRUPT_FILES = False     # recommend leave as true, doesn't add that much time
+BACKUP = True
+CHECK_FOR_CORRUPT_FILES = True
 WRITE_INTERMEDIATE_FILES = False   # True is helpful if you to re-fine tune what each function does
 # ========================
 
@@ -70,8 +71,59 @@ SPEECHNORM_PREFIX = 'speechnorm_'
 NORMALIZE_PREFIX = 'norm_'
 EQUALIZER_PREFIX = 'final_'
 
+
+def override_config_from_file() -> None:
+    """
+    This function exists mostly for the author's edification. You probably don't need a
+    config file.
+
+    I kept forgetting to toggle the default values back to what should be the default so
+    now we're just going to override them from a local config script.
+
+    I can't git skip tree on just part of a file so... here we are.
+
+    If you really feel like you need a config file the format is:
+    {
+        'BACKUP': 'True',
+        'SomeOtherGlobal': '5',
+        'Savvy?': 'Aye',
+    }
+    :return:
+    """
+    config_path = 'resources/config.txt'
+
+    # ensure file exists, make file template if it doesn't
+    if not os.path.exists(config_path):
+        return
+
+    with open(config_path, 'r') as f:
+        config_raw = f.read()
+        try:
+            global_symbol_table = globals()
+            config_globals_dict = literal_eval(config_raw)
+
+            for key in config_globals_dict.keys():
+                # only modify if global exists
+                if key not in global_symbol_table:
+                    break
+
+                # ensure we're importing settings that make sense
+                imported_global_value = literal_eval(config_globals_dict[key])
+                if type(global_symbol_table[key]) != type(imported_global_value):
+                    raise ValueError
+
+                # assign global
+                global_symbol_table[key] = literal_eval(config_globals_dict[key])
+        except SyntaxError or ValueError as e:
+            print(
+                f'\nCredential file at {config_path} is malformed.\nNote: if you delete your file and re-run this program it will remake a sane template.')
+            exit(-1)
+
+
 corrupt_files = []
 def main():
+    override_config_from_file()
+
     t1 = time.time()
     if BACKUP:
         backup_audio_collection()
