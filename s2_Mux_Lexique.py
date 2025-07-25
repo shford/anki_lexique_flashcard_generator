9fe8c564b6611422cc5baa291855f5c87c83cec0
hampton.ford <stevenhford3@gmail.com>
2025-07-25 02:35:20 -0600

    changed return to include associated mux_lemmes. fixed fuzz factor

--- s2_Mux_Lexique.py
+++ s2_Mux_Lexique.py
@@ -1,9 +1,6 @@
 """
 Mux spoken/written and write to file.
 """
-# python core lib
-import os
-
 # external libs
 import pandas as pd
 
@@ -13,33 +10,33 @@ from s1_Filter_Lexique import OUTPUT_CSV as INPUT_CSV
 
 
 # ==== CONFIGURATION ====
-CHUNK_SIZE = 500
 SPOKEN_COUNT = 400
 WRITTEN_COUNT = 100
+MUX_CHUNK_SIZE = SPOKEN_COUNT + WRITTEN_COUNT
 DESIRED_FLASHCARDS = 20000  # set equal to 0 for all available
 OUTPUT_MUXED_CSV = f'{OUTPUT_DIR}/Lexique383 - Muxed.csv'
 # ========================
 
 # Derived Constants
-SELECTED_LEMMES = int(DESIRED_FLASHCARDS * 0.25) # apply fuzz factor
+SELECT_N_LEMMES = int(DESIRED_FLASHCARDS * 1.25) # apply fuzz factor
 
 
 def main():
-    muxed_df = mux_frequencies()
+    muxed_lemmes, muxed_df = mux_frequencies()
     muxed_df.to_csv(OUTPUT_MUXED_CSV, index=False, encoding='utf-8')
-    print("\nDone: Wrote df w/ intermixed by frequencies of spoken/written.")
+    print(f'\nDone: Wrote df w/ {len(muxed_lemmes)} intermixed by frequencies of spoken/written.')
 
 
-def mux_frequencies() -> pd.DataFrame:
+def mux_frequencies() -> (list, pd.DataFrame):
     # load csv
     df_all = pd.read_csv(INPUT_CSV, encoding='utf-8')
 
     # working dataset will be reduced as we process
     df_working = df_all.copy()
 
+    muxed_lemmes = []
     muxed_df = pd.DataFrame()
-    num_lemmes_selected = 0
-    while num_lemmes_selected < SELECTED_LEMMES or SELECTED_LEMMES == 0:
+    while len(muxed_lemmes) < SELECT_N_LEMMES or SELECT_N_LEMMES == 0:
         # top lemmes by spoken and frequency
         highest_spoken = get_top_lemmes(df_working, SPOKEN_COUNT, 'freqlemfilms')
         df_working = df_working[~df_working['lemme'].isin(highest_spoken)]
@@ -52,13 +49,13 @@ def mux_frequencies() -> pd.DataFrame:
         lemme_df.loc[:, '__order'] = pd.Categorical(lemme_df['lemme'], categories=top_lemmes, ordered=True)
         lemme_df = lemme_df.sort_values('__order').drop(columns='__order')
 
-        num_lemmes_selected += len(top_lemmes) # should be equivalent to adding CHUNK_SIZE
+        muxed_lemmes.extend(top_lemmes)
         muxed_df = pd.concat([muxed_df, lemme_df],ignore_index=True)
 
         if not highest_spoken and not highest_written:
             break  # exhausted candidates
 
-    return muxed_df
+    return muxed_lemmes, muxed_df
 
 
 def get_top_lemmes(df, count, freq_col_name) -> list:
