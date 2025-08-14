495c57408eec869de31764765eca679213a87319
hampton.ford <stevenhford3@gmail.com>
2025-08-14 15:55:53 -0600

    tested: removed all references to df_overflow. start and stop are no longer necessary. now much less complex to switch between s3,s4,and s5

--- anki_lexique_flashcard_generator/s4_Enforce_500_Anki_Cards.py
+++ anki_lexique_flashcard_generator/s4_Enforce_500_Anki_Cards.py
@@ -4,80 +4,58 @@
 @Instructions:
   On the first one START should be run. If you run s3 again you should set START to whatever the
   highest filename number (number to the right of '-') plus 1.
-
-  If you don't do this then df_overflow will be prepend to the first file it sees and everything
-  will be shifted down by that many rows.. which isn't a huge deal, it just means your frequencies will be
-  slightly offset.
 """
-import pandas as pd
 import os
+import warnings
 
-from s1_Filter_Lexique import DESIRED_FLASHCARDS as STOP
-from s3_Export_Anki_Format import CHUNK_SIZE
-from s3_Export_Anki_Format import OUTPUT_PREFIX, OVERFLOW_FILENAME, OVERFLOW_PATH
+import pandas as pd
+
 from s3_Export_Anki_Format import ANKI_CSV_OUTPUT_DIR as ANKI_CSV_DIR
-
-# ==== Config Constants ====
-START = 1
-# START = 3001  # example: You create and balanced 1-3000 yesterday, it may it may have generated a df_overflow. You ran s3 today and now have 3001-6000. To rebalance 3001-6000 you would set this to 3001. Note: This program assumes anything in df_overflow at the start of the program is a higher frequency word and will prepend df_overflow and shift words as necessary. This is expected and desireable behavior.
-# ========================
+from s3_Export_Anki_Format import CHUNK_SIZE
+from s3_Export_Anki_Format import OUTPUT_PREFIX
 
 
 def main():
     enforce_chunk_size_anki_cards()
 
 
-def enforce_chunk_size_anki_cards(start=START, stop=STOP):
+def enforce_chunk_size_anki_cards():
     print(f'Enforcing flashcard deck size of {CHUNK_SIZE}...')
-    check_indices(start, stop)
 
     # set up initial state
     df1 = None
-    filename2 = None
     path2 = None
     df2 = None
-    df_overflow = None
-    if os.path.exists(OVERFLOW_PATH):
-        df_overflow = pd.read_csv(OVERFLOW_PATH, encoding='utf-8', header=None)
-    else:
-        df_overflow = pd.DataFrame()
+    df_overflow = pd.DataFrame()
 
-    # begin balancing row count
-    NUM_FILES = int((stop - start + 1) / CHUNK_SIZE)
+    # begin balancing number of rows
+    NUM_FILES = len(os.listdir(ANKI_CSV_DIR))
+    start = 1
     while NUM_FILES >= 1:
+        filename_1 = f'{OUTPUT_PREFIX}{start} - {start + CHUNK_SIZE - 1}.csv'
+        path1 = f'{ANKI_CSV_DIR}/{filename_1}'
         if df1 is None:
-            # init df1 from CSV
-            filename1 = f'{OUTPUT_PREFIX}{start} - {start + CHUNK_SIZE - 1}.csv'
-            path1 = f'{ANKI_CSV_DIR}/{filename1}'
-            try:
-                df1 = pd.read_csv(path1, encoding='utf-8', header=None)
-            except:
-                print(f'No file "{filename1}" for starting lemme {start} found. Tip: Try adjusting starting lemme.')
-                exit(0)
-        else:
-            # set current to next
-            filename1 = filename2
-            path1 = path2
-            df1 = df2
+            df1 = pd.read_csv(path1, encoding='utf-8', header=None)
 
-        if NUM_FILES == 1:
-            # on the last file there is no file2, however to keep the balance logic working we'll make df2 an empty DataFrame
-            df2 = pd.DataFrame()
-        else:
-            # init df2 regardless of first or last run
-            start += CHUNK_SIZE
-            filename2 = f'{OUTPUT_PREFIX}{start} - {start + CHUNK_SIZE - 1}.csv'
-            path2 = f'{ANKI_CSV_DIR}/{filename2}'
+        if NUM_FILES >= 2:
+            filename_2 = f'{OUTPUT_PREFIX}{start + CHUNK_SIZE} - {start + CHUNK_SIZE*2 - 1}.csv'
+            path2 = f'{ANKI_CSV_DIR}/{filename_2}'
             try:
                 df2 = pd.read_csv(path2, encoding='utf-8', header=None)
             except:
+                # the next file DNE -> ignore error and normalize as much as
+                warn_msg = 'WARNING: Likely running s4 without running s3 after increasing the number of DESIRED_FLASHCARDS.'
+                warnings.warn(warn_msg)
                 df2 = pd.DataFrame()
-                NUM_FILES = 1 # clearly the next file DNE (the files < DESIRED_CARDS but that's fine)
-
-        # prepend from df_overflow prior to balancing regardless of length
-        if len(df_overflow) > 0:
-            df1 = pd.concat([df_overflow.iloc[:len(df_overflow)], df1], ignore_index=True)
-            df_overflow = df_overflow.drop(df_overflow.index[:len(df_overflow)]).reset_index(drop=True)
+                NUM_FILES = 1
+        elif NUM_FILES == 1:
+            # on the last file there is no next file,
+            # however to keep the balance logic working we'll make df2 an empty DataFrame
+            df2 = pd.DataFrame()
+            possible_overflow_filename = f'{OUTPUT_PREFIX}{start+CHUNK_SIZE} - {start + CHUNK_SIZE*2 - 1}.csv'
+            possible_overflow_path = f'{ANKI_CSV_DIR}/{possible_overflow_filename}'
+        else:
+            raise FileNotFoundError('Cannot pass negative number of files.')
 
         # region balance rows
         difference = len(df1) - CHUNK_SIZE
@@ -99,76 +77,62 @@ def enforce_chunk_size_anki_cards(start=START, stop=STOP):
 
         # region save ODS documents
         if NUM_FILES == 1:
-            # delete overflow if unneeded
-            if df_overflow.empty and os.path.exists(OVERFLOW_PATH):
-                os.remove(OVERFLOW_PATH)
-
             if not df_overflow.empty:
-                df_overflow.to_csv(OVERFLOW_PATH, index=False, encoding='utf-8', header=False)
-                print('  Saved overflow file for next run.')
-
-            # write current file
-            df1.to_csv(path1, index=False, encoding='utf-8', header=False)
-            print(f'Wrote final .csv: {filename1}')
+                df_overflow.to_csv(possible_overflow_path)
+                if possible_overflow_filename is None:
+                    pass
+                print(f'Wrote .csv: {possible_overflow_filename}')
         elif NUM_FILES == 2 and df2.empty and df_overflow.empty:
-            # delete empty overflow file
-            if os.path.exists(OVERFLOW_PATH):
-                os.remove(OVERFLOW_PATH)
-                print(f'  Deleted empty file {OVERFLOW_FILENAME}.')
-
             # delete empty next file
-            os.remove(path2)
-            print(f'  Deleted empty file {filename2}.')
+            try:
+                os.remove(path2)
+                print(f'  Deleted empty file {filename_2}.')
+            except:
+                pass
 
             # write current file
             df1.to_csv(path1, index=False, encoding='utf-8', header=False)
-            print(f'Wrote final .csv: {filename1}')
+            if filename_1 is None:
+                pass
+            print(f'Wrote .csv: {filename_1}')
 
             # no need for final run
             break
         elif NUM_FILES == 2 and df2.empty:
             # raise Exception - balance logic must be broken
-            raise Exception(
-                "Wha' in tarnation? NUM_FILES==2 and df2.empty but df_overflow is not empty. Balance logic must be broken.")
+            raise Exception("Wha' in tarnation? NUM_FILES==2 and df2.empty but df_overflow is not empty. Balance logic must be broken.")
         elif NUM_FILES == 2 and len(df2) <= 500 and df_overflow.empty:
-            # delete empty overflow file
-            if os.path.exists(OVERFLOW_PATH):
-                os.remove(OVERFLOW_PATH)
-                print(f'Deleted empty file {OVERFLOW_FILENAME}.')
-
-            # write df1
-            df1.to_csv(path1, index=False, encoding='utf-8', header=False)
-            print(f'Wrote: {filename1}')
-
             # write non-empty df2
             df2.to_csv(path2, index=False, encoding='utf-8', header=False)
-            print(f'Wrote final .csv: {filename2}')
+            if filename_2 is None:
+                pass
+            print(f'Wrote .csv: {filename_2}')
+
+            # write current file
+            df1.to_csv(path1, index=False, encoding='utf-8', header=False)
+            if filename_1 is None:
+                pass
+            print(f'Wrote .csv: {filename_1}')
 
             # no need for final run
             break
-        else:
-            # write df1 to .csv file
-            df1.to_csv(path1, index=False, encoding='utf-8', header=False)
-            print(f'Wrote: {filename1}')
         # endregion
+
+        # write current file
+        df1.to_csv(path1, index=False, encoding='utf-8', header=False)
+        if filename_1 is None:
+            pass
+        print(f'Wrote .csv: {filename_1}')
+
+        # set current to next
+        start += CHUNK_SIZE
+        df1 = df2
 
         # decrement loop
         NUM_FILES -= 1
 
     print(f'Enforced deck size.')
     print()
-    return
-
-def check_indices(start, stop):
-    if (stop - start + 1) < 1000:
-        raise Exception('START and STOP are insufficently spaced to open two files.')
-
-    if (start - 1) % 500 != 0:
-        raise Exception('Invalid START - ensure it ends in 1.')
-
-    if stop % 500 != 0:
-        raise Exception('Invalid STOP - ensure it is divisible by 500.')
-
     return
 
 
