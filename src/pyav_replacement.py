05b60915ef6b09955be98669fa8f8e40364df4c5
hampton.ford <stevenhford3@gmail.com>
2025-09-02 01:01:37 -0600

    Still in dev - yep... it's going. like playing wack-a-mole with source code I can't see for a wrapper with questionable implementation and documentation for a C program whose version I don't know for a subject I don't understand... I love this stuff

--- src/pyav_replacement.py
+++ src/pyav_replacement.py
@@ -37,7 +37,7 @@ def main():
 
 
 def measure_loudness_pcm(pcm_bytes, channels, rate, target_lufs=-16, lra=7, tp=-1.5):
-    '''measure EBU R128 loudness on raw PCM bytes'''
+    """measure EBU R128 loudness on raw PCM bytes"""
     cmd = [
         'ffmpeg', '-hide_banner', '-nostdin',
         '-f', 's16le', '-ar', str(rate), '-ac', str(channels),
@@ -45,8 +45,16 @@ def measure_loudness_pcm(pcm_bytes, channels, rate, target_lufs=-16, lra=7, tp=-
         '-af', f'loudnorm=I={target_lufs}:LRA={lra}:TP={tp}:print_format=json',
         '-f', 'null', '-'
     ]
-    p = subprocess.run(cmd, input=pcm_bytes, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
-    stderr = p.stderr.decode('utf-8', errors='ignore')
+
+    try:
+        result = subprocess.run(cmd, input=pcm_bytes, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+        pcm_bytes = result.stdout
+    except subprocess.CalledProcessError as e:
+        print('Stderr:\n')
+        print(e.stderr.decode('utf-8'))
+        raise
+
+    stderr = result.stderr.decode('utf-8', errors='ignore')
     i = stderr.find('{')
     j = stderr.rfind('}')
     if i == -1 or j == -1:
@@ -55,9 +63,9 @@ def measure_loudness_pcm(pcm_bytes, channels, rate, target_lufs=-16, lra=7, tp=-
 
 
 def process_file(filename):
-    # 1) decode input mp3 to raw PCM frames
     input_path = f'{PYAV_TEST_DIR}/{filename}'
     with av.open(input_path) as container:
+        # 1) decode input mp3 to raw PCM frames
         input_stream = container.streams.audio[0]
         a_rate = input_stream.codec_context.sample_rate
         a_channels = input_stream.codec_context.channels
@@ -65,53 +73,104 @@ def process_file(filename):
         a_format = input_stream.codec_context.format
         pcm_frames = [frame for frame in container.decode(audio=0)]
 
-    # 2) first graph: arnndn -> adeclick
-    graph_one = av.filter.Graph()
-    rnnn_model_path = '../resources/rnnoise_models/speech_recording.rnnn'
+        # 2) first graph: arnndn -> adeclick
+        graph_one = av.filter.Graph()
+        rnnn_model_path = '../resources/rnnoise_models/speech_recording.rnnn'
 
-    graph_one.link_nodes(
-        graph_one.add_abuffer(template=input_stream),
-        add_arnndn_to_graph(graph_one, rnnn_model_path),
-        add_adeclick_to_graph(graph_one),
-        graph_one.add('abuffersink'),
-    )
+        graph_one.link_nodes(
+            graph_one.add_abuffer(template=input_stream),
+            add_arnndn_to_graph(graph_one, rnnn_model_path),
+            add_adeclick_to_graph(graph_one),
+            graph_one.add('abuffersink'),
+        ).configure()
 
-    # push frames through graph 1, collect raw PCM in memory
-    tgt_layout = 's16'
-    pcm_bytes = convert_frames_to_pcm_bytes(pcm_frames, tgt_layout, a_layout, a_rate)
+        # 3) push frames through graph one
+        # for frame in container.decode(input_stream):
+        # for frame in pcm_frames:
+        #     graph_one.push(frame)
+        #     while True:
+        #         try:
+        #             p_frame = graph_one.pull()
+        #             if p_frame is None:
+        #                 break
+        #             processed_graph_one_frames.append(p_frame)
+        #         except:
+        #             time.sleep(0.001)
+        processed_frames = []
+        frame_iter = iter(pcm_frames)
+        done_input = False
 
-    # 3) first-pass loudnorm measurement
-    meas = measure_loudness_pcm(pcm_bytes, channels=a_layout, rate=a_rate, target_lufs=LUFS_NORMALIZATION_LEVEL)
-
-    # 4) second graph: loudnorm -> equalizer -> mp3
-    with av.open(output_path, mode='w', format='mp3') as out_mp3:
-        mp3_stream = out_mp3.add_stream('libmp3lame', rate=a_rate, channels=a_layout)
-
-        graph2 = av.filter.Graph()
-        src2 = graph2.add_buffer(template=input_stream)
-        sink2 = graph2.add('abuffersink')
-
-        loudnorm_node = add_loudnorm_to_graph(graph2, )
-        equalizer_node = add_equalizer_to_graph(graph2)
-
-        # link second graph
-        src2.link_to(loudnorm_node)
-        graph2.link_to(equalizer_node)
-        graph2.link_to(sink2)
-        graph2.configure()
-
-        # feed the original PCM frames into the second graph
-        for frame in pcm_frames:
-            graph2.push(frame)
+        def process_frames():
+            has_frames_to_push = True
             while True:
-                f = graph2.pull()
-                if f is None:
-                    break
-                for pkt in mp3_stream.encode(f):
-                    out_mp3.mux(pkt)
-        for pkt in mp3_stream.encode():
-            out_mp3.mux(pkt)
+                # try to push next input frame, if available
+                if has_frames_to_push:
+                    try:
+                        frame = next(frame_iter)
+                        graph_one.push(frame)
+                    except StopIteration:
+                        has_frames_to_push = False
+                        graph_one.push(None)  # signal end of input
+                    except:
+                        # in case of implementation that's not like fsm and not done
+                        # but was actually just blocking
+                        continue
 
+                # poll to pull available frames
+                while True:
+                    try:
+                        f = graph_one.pull()
+                        if f is None and not has_frames_to_push:
+                            # done.
+                            return processed_frames
+                        elif f is not None:
+                            processed_frames.append(f)
+                        break # break if not done - to poll or push more frames
+                    except av.BlockingIOError:
+                        # graph is not ready for more output yet,
+                        # attempt to push more frames (good if like fsm),
+                        # polls if not able to push
+                        break
+                    except av.EOFError:
+                        # some implementations let you know they're done via this error :')
+                        return processed_frames
+
+        processed_frames = process_frames()
+
+        # 4) convert frames to raw PCM and measure loudness
+        tgt_layout = 's16'
+        pcm_bytes = convert_frames_to_pcm_bytes(processed_frames, tgt_layout, a_layout, a_rate)
+
+        # 5) first-pass loudnorm measurement
+        meas = measure_loudness_pcm(pcm_bytes, channels=a_channels, rate=a_rate, target_lufs=LUFS_NORMALIZATION_LEVEL)
+
+        # 6) second graph: loudnorm -> equalizer -> mp3
+        output_path = f'{PYAV_TEST_DIR}/PYAV__{filename}'
+        with av.open(output_path, mode='w', format='mp3') as out_mp3:
+            mp3_stream = out_mp3.add_stream('libmp3lame', rate=a_rate)
+
+            graph_two = av.filter.Graph()
+
+            graph_two.link_nodes(
+                graph_two.add_abuffer(template=input_stream),
+                add_loudnorm_to_graph(graph_two, meas),
+                graph_two.add('abuffersink'),
+            ).configure()
+
+            equalizer_node = add_equalizer_to_graph(graph_two),
+
+            # feed the original PCM frames into the second graph
+            for frame in processed_frames:
+                graph_two.push(frame)
+                while True:
+                    f = graph_two.pull()
+                    if f is None:
+                        break
+                    for pkt in mp3_stream.encode(f):
+                        out_mp3.mux(pkt)
+
+            for pkt in mp3_stream.encode():
+                out_mp3.mux(pkt)
 
 
 def convert_frames_to_pcm_bytes(frames, target_format, target_layout, target_rate):
@@ -162,14 +221,14 @@ def add_equalizer_to_graph(graph):
         node = eq_node
     return node
 
-def add_loudnorm_to_graph(graph, ):
+def add_loudnorm_to_graph(graph, meas):
     ln_args = (
         f'I={LUFS_NORMALIZATION_LEVEL}:LRA=7:TP=-1.5:'
         f'measured_I={meas["input_i"]}:measured_LRA={meas["input_lra"]}:'
         f'measured_TP={meas["input_tp"]}:measured_thresh={meas["input_thresh"]}:'
         f'offset={meas["target_offset"]}:linear=true:print_format=summary'
     )
-    ln_node = graph.add('loudnorm', args=ln_args)
+    return graph.add('loudnorm', args=ln_args)
 
 
 if __name__ == '__main__':
